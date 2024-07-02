2cd8bb6be2493d25a775b1be9ca811cd6b9ad802
rifqirayita8 <rifqirrd8@gmail.com>
2024-07-02 20:58:00 +0700

    gattau

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\DosenController;
 use App\Http\Middleware\AdminRole;
 
 //ApiController
